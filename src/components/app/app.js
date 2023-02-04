aced9e3952b1e4df8d51960147accc8e13866f03
Adiar <datkaevadiar@gmail.com>
2023-02-04 21:52:25 +0600

    state practice

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 function App() {
 
     const data = [
-        { name: 'Maxim B.', salary: 800, increase: false },
-        { name: 'Alex S.', salary: 3000, increase: false },
-        { name: 'Gordon F.', salary: 5000, increase: true },
+        { name: 'Maxim B.', salary: 800, increase: false, id: 1 },
+        { name: 'Alex S.', salary: 3000, increase: false, id: 2 },
+        { name: 'Gordon F.', salary: 5000, increase: true, id: 3 },
 
     ];
 
