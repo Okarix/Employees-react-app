09f2ada0c5d032ef09832180089a04a06a3b7c5f
Adiar <datkaevadiar@gmail.com>
2023-02-07 22:48:11 +0600

    added delete function(used property transfer in the hierarchy and immutability)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,28 +7,53 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: 'Maxim B.', salary: 800, increase: false, id: 1 },
+                { name: 'Alex S.', salary: 3000, increase: false, id: 2 },
+                { name: 'Gordon F.', salary: 5000, increase: true, id: 3 },
 
-    const data = [
-        { name: 'Maxim B.', salary: 800, increase: false, id: 1 },
-        { name: 'Alex S.', salary: 3000, increase: false, id: 2 },
-        { name: 'Gordon F.', salary: 5000, increase: true, id: 3 },
+            ]
+        }
+    }
 
-    ];
+    deleteItem = (id) => {
+        this.setState(({ data }) => {
+            // const index = data.findIndex(elem => elem.id === id);
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1);
+            // const newArr = [...before, ...after]
+            // return {
+            //     data: newArr
+            // }
+            return {
+                data: data.filter(item => item.id !== id)
+            }
 
-    return (
-        <div className="app">
-            <AppInfo />
+        })
+    }
 
-            <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+
+                <EmployeesList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}
+                />
+                <EmployeesAddForm />
             </div>
-
-            <EmployeesList data={data} />
-            <EmployeesAddForm />
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
